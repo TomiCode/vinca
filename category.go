41156f7a0894a0f4721b342b14218755b13f7cf0
Tomasz Król <tomicode@gmail.com>
2019-10-14 18:37:14 +0200

    Save category and container models.

--- category.go
+++ category.go
@@ -10,7 +10,7 @@ type Category struct {
 }
 
 func (v *VincaDatabase) FetchCategories(usr *User) []*Category {
-    rows, err := v.db.Query("select id, title, description, icon from categories where user_id = ?", usr.Id)
+    rows, err := v.db.Query("select id, name, description, icon from categories where user_id = ?", usr.Id)
     if err != nil {
         log.Println("unable to fetch categories for user", usr.Username)
         return nil
@@ -26,4 +26,22 @@ func (v *VincaDatabase) FetchCategories(usr *User) []*Category {
         categories = append(categories, category)
     }
     return categories
+}
+
+func (v *VincaDatabase) SaveCategory(ct *Category, usr *User) error {
+    res, err := v.db.Exec("insert into categories(user_id, name, description, icon) values(?,?,?,?)",
+                        usr.Id, ct.Title, ct.Description, ct.Icon)
+    if err != nil {
+        log.Println("unable to save category to db:", err)
+        return err
+    }
+
+    cid, err := res.LastInsertId()
+    if err != nil {
+        log.Println("unable to fetch id from table.")
+        return nil
+    }
+
+    ct.Id = int(cid)
+    return nil
 }
---
+++ container.go
@@ -0,0 +1,38 @@
+package main
+
+import "log"
+
+type Container struct {
+    Id int `json:"id"`
+    Name string `json:"name"`
+    Certificate []byte `json:"certificate"`
+    Encrypted []byte `json:"encrypted"`
+}
+
+// Currently we support only single containers per user.
+func (v *VincaDatabase) FetchContainer(usr *User) *Container {
+    row := v.db.QueryRow("select id, name, public, encrypted from containers where user_id = ? limit 1", usr.Id)
+
+    var container = &Container{}
+    if err := row.Scan(&container.Id, &container.Name, &container.Certificate, &container.Encrypted); err != nil {
+        log.Println("unable to fetch user container:", err)
+        return nil
+    }
+    return container
+}
+
+func (v *VincaDatabase) SaveContainer(container *Container, usr *User) error {
+    res, err := v.db.Exec("insert into containers(user_id, name, public, encrypted) values(?,?,?,?)",
+            usr.Id, container.Name, container.Certificate, container.Encrypted)
+    if err != nil {
+        log.Println("unable to save container:", err)
+        return err
+    }
+
+    if cid, err := res.LastInsertId(); err == nil {
+        container.Id = int(cid)
+    } else {
+        log.Println("unable to fetch insert id:", err)
+    }
+    return nil
+}
